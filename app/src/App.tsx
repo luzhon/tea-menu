170ef5ebd4cc3e88c589819ca5f4f3a476564188
Lulu Zhong <luzhon@microsoft.com>
2022-02-09 15:21:15 -0800

    davidstea

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TeaList from './TeaList'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <TeaList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/TeaData.ts
@@ -0,0 +1,175 @@
+import {Tea, TeaType, Brand} from "./Tea.types"
+
+export const teaData: Tea[] = [
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwf884f8be/productimages/10179DT01VAR0099357-10179US01VAR0075640-BI-1.jpg',
+    name: 'Forever Nuts',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Apple, Almond, Cinnamon, Beetroot, Artificial sweet roasted almond flavouring.",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwcb1e16e7/productimages/10451DT01VAR0022764-10451US01VAR0017505-BI-1.jpg',
+    name: 'Apple Cider',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Apple, Apple pomace (apple pomace, citric acid), Sweet blackberry leaves, Apple flavouring (artificial apple flavouring, concentrated apple juice), Artificial (vanilla, cream) flavouring",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw9395af90/productimages/10348DT01VAR0015204-10348US01VAR0017249-BI-1.jpg',
+    name: 'Buddha\'s Blend',
+    brand: Brand.DavidsTea,
+    type: [TeaType.White, TeaType.Green],
+    ingredients: "White tea, Green tea, Jasmine pearls, Artificial peach flavouring, White hibiscus blossoms",
+    caffeine: 2,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwd542d446/productimages/10316DT01VAR0015260-10316US01VAR0017421-BI-1.jpg',
+    name: 'Sleigh Ride',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Apple (apple, citric acid), Candied pineapple (pineapple, sugar), Hibiscus blossoms, Candied papaya (papaya, sugar), Beetroot, Cinnamon, Artificial (cinnamon, almond, apple) flavouring, Raisins, Coconut rasps, Roasted almonds (almonds, sugar), Popped rice",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw4c1a9ce8/productimages/10866DT01VAR0099326-10866US01VAR0075635-BI-1.jpg',
+    name: 'Hazelnut Chocolate',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Puer],
+    ingredients: "Pu'erh tea, Apple, Cocoa peel, Artificial chocolate hazelnut flavouring, Chocolate (sugar, cocoa mass, low fat cocoa powder, cocoa butter, sunflower lecithin), Brittle pieces (sugar, hazelnut), Cocoa pieces, Hazelnut, Stevia extract",
+    caffeine: 2,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwb980c9a4/productimages/961651DT01VAR0092728-961651US01VAR0074029-BI-2.jpg',
+    name: 'Santa\'s Secret',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "Black tea, Peppermint, Artificial (vanilla, cream) flavouring",
+    caffeine: 3,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwb769c9ac/productimages/10876DT01VAR0087356-10876US01VAR0066775-BI-1.jpg',
+    name: 'Jingle Bell Chai',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "Black tea, Ginger, Cinnamon, Cocoa nibs, White chocolate (sugar, cocoa butter, whole milk powder, chocolate liquor, whey powder, lactose, soy lecithin, vanilla), Pink peppercorns, Cardamom, Natural white chocolate flavouring, Snowflake candies (sugar, rice flour, vegetable oil (palm, palm kernel), corn starch, gum Arabic, cellulose gum, titanium dioxide, confectioner's glaze, carrageenan, soy lecithin, artificial flavour), Cloves, Cocoa powder, Stevia extract",
+    caffeine: 1,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwf8958a12/productimages/10764DT01VAR0073185-10764US01VAR0056066-BI-1.jpg',
+    name: 'Headache Halo',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Rooibos],
+    ingredients: "Rooibos, Nana mint, Lemongrass, Willow bark, Nettle leaves, Lavender, Passionflower, Vervain leaves, Rose petals, Mallow blossoms",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwd9717c9f/productimages/10431DT01VAR0021037-10431US01VAR0017021-BI-1.jpg',
+    name: 'Chocolate Macaroon',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "Black tea, Toasted coconut (coconut, sugar, dextrose, salt), Coconut, Pecans, Cacao nibs, Rock cane sugar, Natural chocolate flavouring",
+    caffeine: 3,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw4aeba77f/productimages/10041DT01VAR003940-10041US01VAR0012960-BI-1.jpg',
+    name: 'Guangzhou Milk Oolong',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Oolong],
+    ingredients: "Chinese oolong tea from the Fujian province, Natural and artificial milk flavouring",
+    caffeine: 2,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw3e158982/productimages/10794DT01VAR0076719-10794US01VAR0058249-BI-1.jpg',
+    name: 'Dragonfruit Aloe',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Apple, Candied Pineapple, Hibiscus Blossoms, Coconut Chips, Dragonfruit, Candied Aloe Vera, Natural Berry Flavoring, Raspberries",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw05cbd369/productimages/10233DT01VAR004072-10233US01VAR0013092-BI-1.jpg',
+    name: 'Organic Mother\'s Little Helper',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Organic peppermint, Organic lemongrass, Organic hibiscus*, Organic rosehips, Organic chamomile, Organic valerian root, Cornflowers, Natural peppermint flavouring",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw60cf55b5/productimages/10776DT01VAR0074207-10776US01VAR0056938-BI-1.jpg',
+    name: 'Pumpkin Chai',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "Black tea, Cinnamon, Cloves, Caramel (condensed skimmed milk, sugar, glucose syrup, butter, sorbitol, mono and diglycerides), Carrot, Lemon peel, Pumpkin candies (sugar, rice flour, palm and palm kernel oil, corn starch, gum Arabic, cellulose gum, FD&C Yellow 6, confectioner's glaze, carrageenan, soy lecithin, artificial flavouring), Pumpkin flakes, Natural and artificial pumpkin spice flavouring",
+    caffeine: 2,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwca64ec26/productimages/10943DT01VAR0099365-10943US01VAR0075643-BI-1.jpg',
+    name: 'Forever Frosty Tea',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Herbal],
+    ingredients: "Apple, Cinnamon, Almond, Artificial sweet roasted almond and natural orange flavouring, Beetroot, Rock cane sugar, Coconut, Orange peel, Clove",
+    caffeine: 0,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw67754c14/productimages/10921DT01VAR0092098-10921US01VAR0070224-BI-1.jpg',
+    name: 'Mocha Chai',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "100% Elephant Approved black tea, Dark chocolate chips (sugar, chocolate liquor, cocoa butter, soy lecithin, vanilla bean), Roasted yerba mate, Cacao, Chocolate (sugar, Dutch cocoa, unsweetened chocolate, soy lecithin, vanilla), Dark coffee bean, Cinnamon, Clove, Ginger, Natural (cinnamon, clove, milk chocolate) flavouring.",
+    caffeine: 2,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwac134e98/productimages/10521DT01VAR0028642-10521US01VAR0022318-BI-1.jpg',
+    name: 'Cardamom French Toast',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Black],
+    ingredients: "50% Elephant Approved black tea, Cinnamon, Lemon peel, Coriander seeds, Cardamom, Sugar (coconut sugar, cane sugar), Natural and artificial (vanilla, cardamom) flavouring",
+    caffeine: 2,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwb9ff79d5/productimages/10960DT01VAR0095948-10960US01VAR0073000-BI-1.jpg',
+    name: 'White Pumpkin',
+    brand: Brand.DavidsTea,
+    type: [TeaType.White],
+    ingredients: "Apple, Coconut rasp, White chocolate (sugar, cocoa butter, whole milk powder, sunflower lecithin), White tea, Carrot, Natural (pumpkin pie, white chocolate) flavouring, Bean peel, Pumpkin, Pumpkin seed, Sea buckthorn, Stevia extract",
+    caffeine: 1,
+    sealed: true
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dw1a7d3300/productimages/10818DT01VAR0082288-10818US01VAR0062818-BI-1.jpg',
+    name: 'Blood Orange Boost',
+    brand: Brand.DavidsTea,
+    type: [TeaType.Green],
+    ingredients: "Apple (apple, citric acid), Green tea, Hibiscus blossoms, Natural blood orange flavouring, Orange peel, Rosemary, Roasted chicory root, GABA green tea, Olive leaves, Stevia extract",
+    caffeine: 1,
+    sealed: false
+  },
+  {
+    img: 'https://www.davidstea.com/dw/image/v2/BBXZ_PRD/on/demandware.static/-/Sites-davidstea-master-catalog/default/dwae799acf/productimages/10696DT01VAR0060700-10696US01VAR0047090-BI-1.jpg',
+    name: 'Pomegrateful',
+    brand: Brand.DavidsTea,
+    type: [TeaType.White],
+    ingredients: "Apple (apple, citric acid), Pineapple (pineapple, sugar), Hibiscus blossoms, White tea, Natural (pomegranate, cream) flavouring, Pomegranate seeds and blossoms, Rose petals, Stevia extract",
+    caffeine: 1,
+    sealed: true
+  }
+];
